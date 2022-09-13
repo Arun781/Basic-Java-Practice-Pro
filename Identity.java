9d7d1550d082544b002199a8d925b47abe9cec4f
Arun BV <arunbv.xworkz@gmail.com>
2022-09-13 22:59:46 +0530

    Identity Files

---
+++ Identity.java
@@ -0,0 +1,64 @@
+class Identity{ 
+
+static void printName(String name)
+{
+        System.out.println("entered printName");
+        System.out.println("Name entered :"+name);
+}
+static void printlnNameAndEmail(String name, String email)  
+{
+	System.out.println("Enter print Name and Email");
+	System.out.println("Name entered :" +name);
+	System.out.println("Email entered :" +email);
+}
+static void printlNameAndMobile(String name, long Mobile)
+{
+	System.out.println("Enter print Name and Mobile");
+	System.out.println("Name entered" +name);
+	System.out.println("Mobile entered" +Mobile);
+}
+static void printlnNameAndAge(String Name, byte age)
+{
+	System.out.println("Enter print Name and Age");
+	System.out.println("Enter print Name" + Name);
+	System.out.println("Enter print Name and Age"+age);
+}
+static void printlnNameandAddress(String Name, String Address)
+{
+	System.out.println("Enter print Name and Address");
+	System.out.println("Enter print Name"+Name);
+	System.out.println("Enter the Address" +Address);
+}
+static void printlnNameAndEmailAndAddressAndAge(String Name, String Email, int age)
+{
+	System.out.println("Enter print Name "+Name);
+	System.out.println("Enter print Address"+Email);
+	System.out.println("Enter print age"+age);
+}
+static void printlnNameAndGender(String Name, char Gender)
+{
+	System.out.println("Enter print Name"+Name);
+	System.out.println("Enter pirng Gender"+Gender);
+}
+static void printlnNameAndWorking(String Name, String Working)
+{
+	System.out.println("Enter print Name"+Name);
+	System.out.println("Enter print Working"+Working);
+}
+static void printlnNameAndShirtSize(String Name, int ShirtSize)
+{
+	System.out.println("Enter print the Name"+Name);
+	System.out.println("Enter print the ShirtSize"+ShirtSize);
+}
+static void printlnNameAndPantSize(String Name, int PantSize)
+{
+	System.out.println("Enter the Name"+Name);
+	System.out.println("Enter the PantSize"+PantSize);
+}
+static void printlnNameAndWeightAndHeight(String Name, int Weight, float Height)
+{
+		System.out.println("Enter print Name"+Name);
+		System.out.println("Enter print Weight"+Weight);
+		System.out.println("Enter the Height"+Height);
+}
+}
---
+++ IdentityRunner.java
@@ -0,0 +1,148 @@
+class IdentityRunner{
+	public static void main(String [] Names)
+	{
+	  Identity.printName("Arun Veerapur");
+	  Identity.printName("Arun Veerapur");
+	  Identity.printName("Arun Veerapur");
+	  Identity.printName("Arun Veerapur");
+	  Identity.printName("Arun Veerapur");
+	  Identity.printName("Arun Veerapur");
+	  Identity.printName("Arun Veerapur");
+	  Identity.printName("Arun Veerapur");
+	  Identity.printName("Arun Veerapur");
+	  Identity.printName("Arun Veerapur");
+	  Identity.printName("Arun Veerapur");
+	  Identity.printName("Arun Veerapur");
+	  
+	  Identity.printlnNameAndEmail("Arun Veerapur","Arun Veerapur.arunbv.xworkz@gmail.com");
+	  Identity.printlnNameAndEmail("Arun Veerapur","Arun Veerapur.arunbv.xworkz@gmail.com");
+	  Identity.printlnNameAndEmail("Arun Veerapur","Arun Veerapur.arunbv.xworkz@gmail.com");
+	  Identity.printlnNameAndEmail("Arun Veerapur","Arun Veerapur.arunbv.xworkz@gmail.com");
+	  Identity.printlnNameAndEmail("Arun Veerapur","Arun Veerapur.arunbv.xworkz@gmail.com");
+	  Identity.printlnNameAndEmail("Arun Veerapur","Arun Veerapur.arunbv.xworkz@gmail.com");
+	  Identity.printlnNameAndEmail("Arun Veerapur","Arun Veerapur.arunbv.xworkz@gmail.com");
+	  Identity.printlnNameAndEmail("Arun Veerapur","Arun Veerapur.arunbv.xworkz@gmail.com");
+	  Identity.printlnNameAndEmail("Arun Veerapur","Arun Veerapur.arunbv.xworkz@gmail.com");
+	  Identity.printlnNameAndEmail("Arun Veerapur","Arun Veerapur.arunbv.xworkz@gmail.com");
+	  Identity.printlnNameAndEmail("Arun Veerapur","Arun Veerapur.arunbv.xworkz@gmail.com");
+	  Identity.printlnNameAndEmail("Arun Veerapur","Arun Veerapur.arunbv.xworkz@gmail.com");
+	  
+	  Identity.printlNameAndMobile("Arun Veerapur",9620753670L);
+	  Identity.printlNameAndMobile("Arun Veerapur",9620753670L);
+	  Identity.printlNameAndMobile("Arun Veerapur",9620753670L);
+	  Identity.printlNameAndMobile("Arun Veerapur",9620753670L);
+	  Identity.printlNameAndMobile("Arun Veerapur",9620753670L);
+	  Identity.printlNameAndMobile("Arun Veerapur",9620753670L);
+	  Identity.printlNameAndMobile("Arun Veerapur",9620753670L);
+	  Identity.printlNameAndMobile("Arun Veerapur",9620753670L);
+	  Identity.printlNameAndMobile("Arun Veerapur",9620753670L);
+	  Identity.printlNameAndMobile("Arun Veerapur",9620753670L);
+	  Identity.printlNameAndMobile("Arun Veerapur",9620753670L);
+	  Identity.printlNameAndMobile("Arun Veerapur",9620753670L);
+	  
+	  Identity.printlnNameAndAge("Arun Veerapur",(byte)24);
+	  Identity.printlnNameAndAge("Arun Veerapur",(byte)24);
+	  Identity.printlnNameAndAge("Arun Veerapur",(byte)24);
+	  Identity.printlnNameAndAge("Arun Veerapur",(byte)24);
+	  Identity.printlnNameAndAge("Arun Veerapur",(byte)24);
+	  Identity.printlnNameAndAge("Arun Veerapur",(byte)24);
+	  Identity.printlnNameAndAge("Arun Veerapur",(byte)24);
+	  Identity.printlnNameAndAge("Arun Veerapur",(byte)24);
+	  Identity.printlnNameAndAge("Arun Veerapur",(byte)24);
+	  Identity.printlnNameAndAge("Arun Veerapur",(byte)24);
+	  Identity.printlnNameAndAge("Arun Veerapur",(byte)24);
+	  Identity.printlnNameAndAge("Arun Veerapur",(byte)24);
+	  
+	  Identity.printlnNameandAddress("Arun Veerapur", "Near Hanuman Temple Main Bazar");
+	  Identity.printlnNameandAddress("Arun Veerapur", "Near Hanuman Temple Main Bazar");
+	  Identity.printlnNameandAddress("Arun Veerapur", "Near Hanuman Temple Main Bazar");
+	  Identity.printlnNameandAddress("Arun Veerapur", "Near Hanuman Temple Main Bazar");
+	  Identity.printlnNameandAddress("Arun Veerapur", "Near Hanuman Temple Main Bazar");
+	  Identity.printlnNameandAddress("Arun Veerapur", "Near Hanuman Temple Main Bazar");
+	  Identity.printlnNameandAddress("Arun Veerapur", "Near Hanuman Temple Main Bazar");
+	  Identity.printlnNameandAddress("Arun Veerapur", "Near Hanuman Temple Main Bazar");
+	  Identity.printlnNameandAddress("Arun Veerapur", "Near Hanuman Temple Main Bazar");
+	  Identity.printlnNameandAddress("Arun Veerapur", "Near Hanuman Temple Main Bazar");
+	  Identity.printlnNameandAddress("Arun Veerapur", "Near Hanuman Temple Main Bazar");
+	  Identity.printlnNameandAddress("Arun Veerapur", "Near Hanuman Temple Main Bazar");
+	  
+	  Identity.printlnNameAndEmailAndAddressAndAge("Arun Veerapur", "arunbv9999@gmail.com", (int)25);
+	  Identity.printlnNameAndEmailAndAddressAndAge("Arun Veerapur", "arunbv9999@gmail.com", (int)25);
+	  Identity.printlnNameAndEmailAndAddressAndAge("Arun Veerapur", "arunbv9999@gmail.com", (int)25);
+	  Identity.printlnNameAndEmailAndAddressAndAge("Arun Veerapur", "arunbv9999@gmail.com", (int)25);
+	  Identity.printlnNameAndEmailAndAddressAndAge("Arun Veerapur", "arunbv9999@gmail.com", (int)25);
+	  Identity.printlnNameAndEmailAndAddressAndAge("Arun Veerapur", "arunbv9999@gmail.com", (int)25);
+	  Identity.printlnNameAndEmailAndAddressAndAge("Arun Veerapur", "arunbv9999@gmail.com", (int)25);
+	  Identity.printlnNameAndEmailAndAddressAndAge("Arun Veerapur", "arunbv9999@gmail.com", (int)25);
+	  Identity.printlnNameAndEmailAndAddressAndAge("Arun Veerapur", "arunbv9999@gmail.com", (int)25);
+	  Identity.printlnNameAndEmailAndAddressAndAge("Arun Veerapur", "arunbv9999@gmail.com", (int)25);
+	  Identity.printlnNameAndEmailAndAddressAndAge("Arun Veerapur", "arunbv9999@gmail.com", (int)25);
+	  Identity.printlnNameAndEmailAndAddressAndAge("Arun Veerapur", "arunbv9999@gmail.com", (int)25);
+	  
+	  Identity.printlnNameAndGender("Arun Veerapur", 'M');
+	  Identity.printlnNameAndGender("Arun Veerapur", 'M');
+	  Identity.printlnNameAndGender("Arun Veerapur", 'M');
+	  Identity.printlnNameAndGender("Arun Veerapur", 'M');
+	  Identity.printlnNameAndGender("Arun Veerapur", 'M');
+	  Identity.printlnNameAndGender("Arun Veerapur", 'M');
+	  Identity.printlnNameAndGender("Arun Veerapur", 'M');
+	  Identity.printlnNameAndGender("Arun Veerapur", 'M');
+	  Identity.printlnNameAndGender("Arun Veerapur", 'M');
+	  Identity.printlnNameAndGender("Arun Veerapur", 'M');
+	  Identity.printlnNameAndGender("Arun Veerapur", 'M');
+	  Identity.printlnNameAndGender("Arun Veerapur", 'M');
+	  
+	  Identity.printlnNameAndWorking("Arun Veerapur", "X-Workz");
+	  Identity.printlnNameAndWorking("Arun Veerapur", "X-Workz");
+	  Identity.printlnNameAndWorking("Arun Veerapur", "X-Workz");
+	  Identity.printlnNameAndWorking("Arun Veerapur", "X-Workz");
+	  Identity.printlnNameAndWorking("Arun Veerapur", "X-Workz");
+	  Identity.printlnNameAndWorking("Arun Veerapur", "X-Workz");
+	  Identity.printlnNameAndWorking("Arun Veerapur", "X-Workz");
+	  Identity.printlnNameAndWorking("Arun Veerapur", "X-Workz");
+	  Identity.printlnNameAndWorking("Arun Veerapur", "X-Workz");
+	  Identity.printlnNameAndWorking("Arun Veerapur", "X-Workz");
+	  Identity.printlnNameAndWorking("Arun Veerapur", "X-Workz");
+	  Identity.printlnNameAndWorking("Arun Veerapur", "X-Workz");
+	  
+	  Identity.printlnNameAndShirtSize("Arun Veerapur", (int)34);
+	  Identity.printlnNameAndShirtSize("Arun Veerapur", (int)34);
+	  Identity.printlnNameAndShirtSize("Arun Veerapur", (int)34);
+	  Identity.printlnNameAndShirtSize("Arun Veerapur", (int)34);
+	  Identity.printlnNameAndShirtSize("Arun Veerapur", (int)34);
+	  Identity.printlnNameAndShirtSize("Arun Veerapur", (int)34);
+	  Identity.printlnNameAndShirtSize("Arun Veerapur", (int)34);
+	  Identity.printlnNameAndShirtSize("Arun Veerapur", (int)34);
+	  Identity.printlnNameAndShirtSize("Arun Veerapur", (int)34);
+	  Identity.printlnNameAndShirtSize("Arun Veerapur", (int)34);
+	  Identity.printlnNameAndShirtSize("Arun Veerapur", (int)34);
+	  Identity.printlnNameAndShirtSize("Arun Veerapur", (int)34);
+	  
+	  Identity.printlnNameAndPantSize("Arun Veerapur", (int)32);
+	  Identity.printlnNameAndPantSize("Arun Veerapur", (int)32);
+	  Identity.printlnNameAndPantSize("Arun Veerapur", (int)32);
+	  Identity.printlnNameAndPantSize("Arun Veerapur", (int)32);
+	  Identity.printlnNameAndPantSize("Arun Veerapur", (int)32);
+	  Identity.printlnNameAndPantSize("Arun Veerapur", (int)32);
+	  Identity.printlnNameAndPantSize("Arun Veerapur", (int)32);
+	  Identity.printlnNameAndPantSize("Arun Veerapur", (int)32);
+	  Identity.printlnNameAndPantSize("Arun Veerapur", (int)32);
+	  Identity.printlnNameAndPantSize("Arun Veerapur", (int)32);
+	  Identity.printlnNameAndPantSize("Arun Veerapur", (int)32);
+	  Identity.printlnNameAndPantSize("Arun Veerapur", (int)32);
+	  
+	  Identity.printlnNameAndWeightAndHeight("Arun Veerapur", (int)68,(float)5.8);
+	  Identity.printlnNameAndWeightAndHeight("Arun Veerapur", (int)68,(float)5.8);
+	  Identity.printlnNameAndWeightAndHeight("Arun Veerapur", (int)68,(float)5.8);
+	  Identity.printlnNameAndWeightAndHeight("Arun Veerapur", (int)68,(float)5.8);
+	  Identity.printlnNameAndWeightAndHeight("Arun Veerapur", (int)68,(float)5.8);
+	  Identity.printlnNameAndWeightAndHeight("Arun Veerapur", (int)68,(float)5.8);
+	  Identity.printlnNameAndWeightAndHeight("Arun Veerapur", (int)68,(float)5.8);
+	  Identity.printlnNameAndWeightAndHeight("Arun Veerapur", (int)68,(float)5.8);
+	  Identity.printlnNameAndWeightAndHeight("Arun Veerapur", (int)68,(float)5.8);
+	  Identity.printlnNameAndWeightAndHeight("Arun Veerapur", (int)68,(float)5.8);
+	  Identity.printlnNameAndWeightAndHeight("Arun Veerapur", (int)68,(float)5.8);
+	  Identity.printlnNameAndWeightAndHeight("Arun Veerapur", (int)68,(float)5.8);
+	  
+	}
+}
