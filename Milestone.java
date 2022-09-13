b44c48ac8d68ea2071263dc7412617f3fe77b8a9
Arun BV <arunbv.xworkz@gmail.com>
2022-09-13 10:23:24 +0530

    Helo Arun

---
+++ Milestone.java
@@ -0,0 +1,27 @@
+class Milestone
+{
+	static void TheAgesOfResponsibilities()
+	{
+		int ages = 10;
+		String gender = "Female";
+		if (ages ==10 )
+		{
+			System.out.println("The Responsibilities is go the school");
+			return;
+		}
+		
+		if (ages = 16)
+		{
+			System.out.println("The Responsibilities is go the school");
+			return;
+		}
+		
+		else (ages = 18)
+		{
+			System.out.println("The Girl is ligally can marry");
+			System.out.println("The Responsibilities is to make Dl");
+			System.out.println("The Responsibilities is to make Voter ID");
+			
+		};
+	}
+}
---
+++ MilestoneRunnner.java
@@ -0,0 +1,8 @@
+class MilestoneRunnner{
+	public static void main (String [] ages)
+	{        
+		Milestone.TheAgesOfResponsibilities();
+		System.out.println("Declaring the Ages Of responsibilities");
+			
+	}
+}
